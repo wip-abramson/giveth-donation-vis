7d6a92d3df4c1a7b005e9d7e20f4e9315617798d
geleeroyale <krassvs@gmail.com>
2019-09-22 23:38:52 +0200

    change title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import HelpIcon from '@material-ui/icons/Help'
 const Header = ({ onHelp }) => (
   <Grid container direction="row" alignItems="center" spacing={16}>
     <Grid item>
-      <Typography variant="title">The Graph ETHDenver dApp</Typography>
+      <Typography variant="title">Giveth Donations Dashboard</Typography>
     </Grid>
     <Grid item>
       <IconButton
